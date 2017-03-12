0b8a3cf2ee592fe34b6170d5a2ac1f5756b6fd0a
ebolatumor <komlosi.attila@gmail.com>
2017-03-12 01:49:18 +0100

    commitka

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import Game from './Game';
 import EventEmitter from 'event-emitter';
 window.initGame = function() {
 	const eventEmitter = EventEmitter({});
-	const game = new Game(input, eventEmitter);
+	const game = new Game(getRandomInput(input), eventEmitter);
+}
+
+function getRandomInput(input) {
+	let ran = Math.floor(Math.random() * input.places.length);
+	return input.places[ran];
 }
